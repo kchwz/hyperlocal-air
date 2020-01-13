1a4f77b47936613613394d11146da5f761e849fa
Kai <kaic1121351@gmail.com>
2020-01-12 17:49:30 -0800

    "yeet my feet"

--- main.py
+++ main.py
@@ -25,46 +25,52 @@ L_CS = 29
 reset = Pin(27, mode=Pin.OUT)
 set_active = Pin(14, mode=Pin.OUT)
 set_active(0)
+set_active(1)
+reset(0)
+time.sleep(5.5)
+reset(1)
+time.sleep(10)
 
 def read():
-    while True:
-        set_active(1)
-        reset(0)
-        time.sleep(5.5)
-        reset(1)
-        time.sleep(5.5)
-        #checksum = 0x42+0x4D+0xE4+0x00+0x00
-        #serCmd = struct.pack('!BBBBBH',0x42,0x4D,0xE4,0x00,0x00,checksum)
-        firstByte = uart.read(1)
-        print(firstByte)
-        secondByte = uart.read(1)
-        print(secondByte)
-        read_buffer = 0
-        read_buffer = uart.read(30)
+    read_buffer = 0
+    read_buffer = uart.read()
+    if read_buffer is None:
+        return None
+    else:
         print(read_buffer)
-        data = struct.unpack('!BBBBBBBBBBBBBBBBBBBBBBBBBBBBBB', read_buffer)
+    data = struct.unpack('!BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB', read_buffer)
 
-        return {
-                'H_Length': data[H_Length],
-                'L_Length': data[L_Length],
-                'H_D1': data[H_D1],
-                'L_D1': data[L_D1],
-                'H_D2': data[H_D2],
-                'L_D2': data[L_D2],
-                'H_D3': data[H_D3],
-                'L_D3': data[L_D3],
-                'H_D4': data[H_D4],
-                'L_D4': data[L_D4],
-                'H_D5': data[H_D5],
-                'L_D5': data[L_D5],
-                'H_D6': data[H_D6],
-                'L_D6': data[L_D6],
-                'H_D13': data[H_D13],
-                'L_D13': data[L_D13],
-                'H_CS': data[H_CS],
-                'L_CS': data[L_CS],
-                }
+    return {
+            'H_Length': data[H_Length+2],
+            'L_Length': data[L_Length+2],
+            'H_D1': data[H_D1+2],
+            'L_D1': data[L_D1+2],
+            'H_D2': data[H_D2+2],
+            'L_D2': data[L_D2+2],
+            'H_D3': data[H_D3+2],
+            'L_D3': data[L_D3+2],
+            'H_D4': data[H_D4+2],
+            'L_D4': data[L_D4+2],
+            'H_D5': data[H_D5+2],
+            'L_D5': data[L_D5+2],
+            'H_D6': data[H_D6+2],
+            'L_D6': data[L_D6+2],
+            'H_D13': data[H_D13+2],
+            'L_D13': data[L_D13+2],
+            'H_CS': data[H_CS+2],
+            'L_CS': data[L_CS+2],
+            }
 count = 0
-while(count < 3):
+while(count < 10000):
     data = read()
-    print(data)
+    if data is not None:
+        PM1_S = data['H_D1']*256+data['L_D1']
+        PM2_5_S = data['H_D2']*256+data['L_D2']
+        PM10_S = data['H_D3']*256+data['L_D3']
+        PM1_E = data['H_D4']*256+data['L_D4']
+        PM2_5_E = data['H_D5']*256+data['L_D5']
+        PM10_E = data['H_D6']*256+data['L_D6']
+        print((PM1_S, PM2_5_S, PM10_S))
+        print((PM1_E, PM2_5_E, PM10_E))
+    count+=1
+    time.sleep(0.001)
